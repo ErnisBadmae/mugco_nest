711704200425a1bb725b4f2197dfed3de3e7aad8
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-05-17 09:15:51 +0300

    feat: create dto Users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,44 @@
-import { Controller } from '@nestjs/common';
-import { UserService } from './user.service';
+import { Controller, Get, Patch, Put } from '@nestjs/common'
+import { UserService } from './user.service'
+import { Auth } from 'src/auth/decorators/auth.decorator'
+import { CurrentUser } from 'src/auth/decorators/user.decorator'
+import {
+	Body,
+	Controller,
+	HttpCode,
+	Post,
+	UsePipes,
+	ValidationPipe
+} from '@nestjs/common'
+import { userDto } from './user.dto'
 
-@Controller('user')
+@Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+	constructor(private readonly userService: UserService) {}
+
+  @Get('profile')
+	@Auth()
+	async getProfile(@CurrentUser('id') id: number) {
+    return this.userService.byId(id)
+	}
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Auth()
+  @Put('profile')
+  async getNewTokens(@CurrentUser('id') id: number, @Body(),
+  dto:userDto) {
+    return this.userService.updateProfile(id, dto)
+  }
+
+
+
+  @Auth()
+  @HttpCode(200)
+  @Patch('profile/favorites/:productId')
+  async toggleFavorite(@CurrentUser('id') id: number, @Body(),
+  dto:userDto) {
+    return this.userService.updateProfile(id, dto)
+  }
+
 }
