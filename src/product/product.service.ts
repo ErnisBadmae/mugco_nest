17b51c7b9862e149d05dacb066edf690c20742e4
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-05-24 13:05:06 +0300

    feat: services

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,12 +1,77 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { CategoryDto } from 'src/category/category.dto'
+import { returnCategoryObject } from 'src/category/return-category.object'
+import { PrismaService } from 'src/prisma.service'
+import { generateSlug } from 'src/utils/generate-slug'
 
 @Injectable()
 export class ProductService {
-	// create(createProductDto: CreateProductDto) {
-	// 	return 'This action add a new Product'
-	// }
+	constructor(private prisma: PrismaService) {}
 
-	findAll() {
-		return `This action return all products`
+	async byId(id: number) {
+		if (isNaN(id)) {
+			throw new Error(`Invalid id: ${id}`)
+		}
+
+		const category = await this.prisma.category.findUnique({
+			where: {
+				id
+			},
+
+			select: returnCategoryObject
+		})
+		if (!category) {
+			throw new NotFoundException('Category not found')
+		}
+		return category
+	}
+
+	async create() {
+		return this.prisma.category.create({
+			data: {
+				name: '',
+				slug: ''
+			}
+		})
+	}
+
+	async bySlug(slug: string) {
+		const category = await this.prisma.category.findUnique({
+			where: {
+				slug
+			},
+
+			select: returnCategoryObject
+		})
+		if (!category) {
+			throw new NotFoundException('Category not found')
+		}
+		return category
+	}
+
+	async update(id: number, dto: CategoryDto) {
+		const category = await this.byId(id)
+
+		if (!category) {
+			throw new NotFoundException(`Category with ID ${id} not found.`)
+		}
+
+		return this.prisma.category.update({
+			where: {
+				id
+			},
+			data: {
+				name: dto.name,
+				slug: generateSlug(dto.name)
+			}
+		})
+	}
+
+	async delete(id: number) {
+		return this.prisma.category.delete({
+			where: {
+				id
+			}
+		})
 	}
 }
